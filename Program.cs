c674547d9681cec9ed4f073288762b936ed60a9d
Администратор <@DESKTOP-PHORFLN>
2020-09-24 08:54:39 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Collections;
+using System.Collections.Specialized;
+
+namespace Практика4._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+ 
+            ListDictionary list = new ListDictionary(new CaseInsensitiveComparer(CultureInfo.InvariantCulture));
+
+            list["Estados Unidos"] = "United States of America";
+            list["Canada"] = "Canada";
+            list["Espana"] = "Spain";
+ 
+            Console.WriteLine(list["espana"]);
+            Console.WriteLine(list["CANADA"]);
+            Console.WriteLine(list["eStados unidos"]);
+            Console.Read();
+
+        }
+    }
+}
